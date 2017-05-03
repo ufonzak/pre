ab5a6b78a08890f9210cfbeed051ff8432252617
ufonzak <ufonzak@gmail.com>
2017-05-03 14:47:04 +0200

    Adding router

--- src/client/components/App.jsx
+++ src/client/components/App.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
-import Greeting from './Greeting';
+import Router from './Router';
 
 export default class App extends React.Component { // eslint-disable-line react/prefer-stateless-function
+  componentDidMount() {
+    console.log('fetching...');
+    const test = async () => {
+      const response = await fetch('/hello');
+      const data = await response.json();
+      this._data = data;
+      console.log(data);
+    };
+    test();
+  }
+
   render() {
-    return (<Greeting />);
+    return (<Router />);
   }
 }
---
+++ src/client/components/Router.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+} from 'react-router-dom';
+import Hdo from './Hdo';
+import Home from './Home';
+import Settings from './Settings';
+
+export default () => (
+  <Router>
+    <div>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/hdo">Hdo</Link></li>
+        <li><Link to="/settings">Settings</Link></li>
+      </ul>
+
+      <hr />
+
+      <Route exact path="/" component={Home} />
+      <Route path="/hdo" component={Hdo} />
+      <Route path="/settings" component={Settings} />
+    </div>
+  </Router>
+);
