f94be4d0852cea6982b98a4bf2006f03914b8a3e
ufonzak <ufonzak@gmail.com>
2017-05-09 11:24:49 +0200

    fixing a small issue

--- src/client/sagas/rootSaga.js
+++ src/client/sagas/rootSaga.js
@@ -1,9 +1,11 @@
+import { call } from 'redux-saga/effects';
+
 import settingsSaga from './settings';
 import hdoSaga from './hdo';
 
 export default function* rootSaga() {
   yield [
-    settingsSaga(),
-    hdoSaga(),
+    call(settingsSaga),
+    call(hdoSaga),
   ];
 }
